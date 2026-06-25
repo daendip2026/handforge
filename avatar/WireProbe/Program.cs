44b0430afc5989ddec59438da447463757e8b9c0
daendip2026 <daendip2026@gmail.com>
2026-06-25 20:01:30 +0900

    feat(avatar): add WireProbe prototype to verify UDP protobuf connection

---
+++ avatar/WireProbe/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+int port = 9000;
+using var receiver = new UdpClient(port);
+Console.WriteLine($"Listening on port {port}");
+
+while(true){
+    var result = await receiver.ReceiveAsync();
+    var data = result.Buffer;
+    var frame = HandForge.Proto.Frame.Parser.ParseFrom(data);
+
+    Console.WriteLine($"Received frame {frame.FrameIndex}");
+    
+    foreach(var hand in frame.Hands){
+        Console.WriteLine($"Hand {hand.Handedness}");
+        foreach(var landmark in hand.Landmarks){
+            Console.WriteLine($"Landmark {landmark.X} {landmark.Y} {landmark.Z}");
+        }
+    }
+}   
